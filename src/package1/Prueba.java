4ed4f2074f58837c7744cecbaae10ea1edb287a2
Oda <aodalyz@gmail.com>
2023-08-12 11:11:03 -0500

    Creacion main

---
+++ src/package1/Prueba.java
@@ -0,0 +1,14 @@
+package src.package1;
+
+import javax.sound.sampled.AudioSystem;
+
+public class Prueba {
+
+    Prueba (String cualquiera){
+
+    }
+
+    void test () {
+        System.out.printf("Cualquier String");
+    }
+}
---
+++ src/package2/Main.java
@@ -0,0 +1,11 @@
+package src.package2;
+
+import src.package1.Prueba;
+
+public class Main {
+
+    public static void main (String [] arg){
+        Prueba prueba = new Prueba();
+        System.out.println("Aranque!");
+}
+}
