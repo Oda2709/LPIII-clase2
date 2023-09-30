815bd2108eaf6b8e6dbc333b134f36400f806c0e
Oda2709 <A01-1-0503-02@miremington.local>
2023-09-30 11:10:07 -0500

    Final

--- Parte 2/src/collection/EjemploMap.java
+++ Parte 2/src/collection/EjemploMap.java
@@ -1,4 +1,27 @@
 package collection;
 
-public class EjemploMap {
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EjemploMap { // el mapa no es una coleccion
+    public static void main(String[] args) {
+        Map <Integer, String> map = new HashMap<>();
+        map.put(4, "AccionEliminar");
+        map.put(3, "AccionMostrar");
+       // System.out.println(map); // control + d duplica la linea  -- control shit muevo las lineas --
+        // clic derecho refactor name
+        map.put(20, "Accioncualquiera");
+        map.put(11, "Accioncualquiera");
+        map.put(11, "Accioncualquiera5");  // si ya existe lo sobreescribe
+        //System.out.println(map);
+        String value2o = map.get(20);
+      //  System.out.println(value2o);
+        String value2 = map.get(11);
+        //System.out.println(value2);
+
+        map.forEach((key, value) ->System.out.println("Key:" + key + "->" + "Value:" + value));
+
+
+    }
 }
--- Parte 2/src/collection/EjemploSet.java
+++ Parte 2/src/collection/EjemploSet.java
@@ -3,19 +3,24 @@ package collection;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EjemploSet {
     public static void main(String[] args) {
-        Set<Integer> integers = new LinkedHashSet<>(); // lo inserta al final
+        Set<Integer> integers = new TreeSet<>(); //ordena
+       // Set<Integer> integers = new LinkedHashSet<>(); // lo inserta al final
         integers.add(1);
         integers.add(5);
-        System.out.println(integers);
+        //System.out.println(integers);
         integers.add(5);
-        System.out.println(integers);
+        //System.out.println(integers);
         integers.addAll(Set.of(5,1,2,3,8,20,24,65));
-        System.out.println(integers);
+        //System.out.println(integers);
         integers.add(25);
-        System.out.println(integers);
+        //System.out.println(integers);
+        integers.remove(5);
+
+        integers.forEach(integer -> System.out.println(integer));
 
         //Set<Integer> integers = new HashSet<>(); // Lo inserta como le da la gana
     }
